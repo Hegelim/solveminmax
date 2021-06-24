3e95bb2223bc86527e551a8afffa032331948c63
周烨文（Yewen Zhou） <zhouyewen_sx@qiyi.com>
2021-06-24 18:55:45 +0800

    Rewrite with sympy and it returns exact values

---
+++ src/package/solver.py
@@ -0,0 +1,75 @@
+import re
+from sympy import N
+from sympy.solvers import solve
+from sympy import Symbol
+from itertools import product
+
+
+def convert_letters(matchobj):
+    m = matchobj.group()
+    match_map = {m: "*result[0]"} if len(m) == 2 else {m: m}
+    return match_map[m]
+
+
+# def knit_str(equation):
+#     eq_replaced = re.sub(r"\*([a-z]+)", convert_letters, equation)
+#     eq_replaced = re.sub(r"\d+", convert_digits, eq_replaced)
+#     eq_replaced = (eq_replaced.replace("min", "np.minimum")
+#                    .replace("max", "np.maximum"))
+#     return eq_replaced
+#
+#
+# def extract_eq_val(equation_str):
+#     m = re.search(r"([^=]+)=\s+(\d+)", equation_str)
+#     if m:
+#         return m.group(1), float(m.group(2))
+
+
+def solve_sum_minmax(equation, low=0, high=1.0, float=False):
+    """Solve a sum of min equations numerically.
+
+    Args:
+        equation: string of min equation
+        low: float, the lower bound of the variable, exclusive
+        high: float, the higher bound of the variable, exclusive
+        float: bool,whether to return exact float value
+    """
+    equation = f"+{equation}"
+    value = re.search(r"=\s*(\d+)", equation).group(1)
+    minmax_parts = re.findall(r"(\+|\-)\s*(min|max)(\([^\)]+\))", equation)
+    digit_parts = re.findall(r"((\+|\-)\s*\d+(\*[a-z])+)", equation)
+
+    lists = []
+    for eq in minmax_parts:
+        match = re.findall(r"(\d+[\*a-z]*),\s+(\d+[\*a-z]*)", eq[2])
+        lists.append(match[0])
+    products = product(*lists)
+
+    a_replace = re.sub(r"\*([a-z]+)", convert_letters, equation)
+    a_replace = a_replace.replace(r"=", "==")
+    a = Symbol("a")
+
+    for p in products:
+        knit = ""
+        for digit_part in digit_parts:
+            knit += digit_part[0]
+        for i, operand in enumerate(p):
+            operator = minmax_parts[i][0]
+            knit += f"{operator}{operand}"
+        knit += f"-{value}"
+        result = solve(eval(knit), a)
+        if len(result) == 0:
+            continue
+        elif (result[0] <= low) or (result[0]) >= high:
+            continue
+        if eval(a_replace):
+            if float:
+                return N(result[0])
+            else:
+                return result[0]
+    return None
+
+
+if __name__ == "__main__":
+    eq = "min(500, 600*a) - max(400, 500*a) = 200"
+    print(solve_sum_minmax(eq))
--- tests/test_solve_sum_minmax.py
+++ tests/test_solve_sum_minmax.py
@@ -1,41 +1,38 @@
+from src.package import solver
 import numpy as np
-
-from src.package import solve
+from sympy import *
 
 
 class TestClass:
     def test_1(self):
-        eq = 'min(400, 400*a) + min(400, 400*a) + min(0, 100*a)'
-        value = 200
-        assert solve.solve_sum_minmax(eq, value) == 0.25
+        eq = 'min(400, 400*a) + min(400, 400*a) + min(0, 100*a) = 200'
+        assert solver.solve_sum_minmax(eq) == 0.25
 
     def test_2(self):
         """If only has one equation. """
-        eq = "min(400, 400*a)"
-        value = 400
-        assert solve.solve_sum_minmax(eq, value) is None
+        eq = "min(400, 400*a) = 400"
+        assert solver.solve_sum_minmax(eq) is None
 
     def test_3(self):
-        eq = "min(400, 500*a)"
-        value = 300
-        assert solve.solve_sum_minmax(eq, value) == 0.6
+        eq = "min(400, 500*a) = 300"
+        assert solver.solve_sum_minmax(eq).evalf() == 0.6
 
     def test_4(self):
         """If a is on the left. """
-        eq = "min(500*a, 400)"
-        value = 300
-        assert solve.solve_sum_minmax(eq, value) == 0.6
+        eq = "min(500*a, 400) = 300"
+        assert solver.solve_sum_minmax(eq).evalf() == 0.6
 
     def test_5(self):
         """If have constants before min. """
-        eq = "800*a + 2*min(300, 400*a)"
-        value = 1000
-        assert solve.solve_sum_minmax(eq, value) == 0.625
+        eq = "800*a + min(300, 400*a) + min(300, 400*a) = 1000"
+        assert solver.solve_sum_minmax(eq).evalf() == 0.625
 
     def test_6(self):
         """If the equation has both min and max. """
-        eq = "min(500, 600*a) + max(400, 500*a)"
-        value = 500
-        assert solve.solve_sum_minmax(eq, value) == 0.16666
+        eq = "min(500, 600*a) + max(400, 500*a) = 500"
+        assert solver.solve_sum_minmax(eq).evalf() == 1/6
 
+    # def test_7(self):
+    #     eq = "min(500, 600*a) - min(500, 600*a) = 0"
+    #     assert solver.solve_sum_minmax(eq).evalf() ==
 
