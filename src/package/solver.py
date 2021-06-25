666173ed89a8b946cd700ebb3e3b9e168170e175
周烨文（Yewen Zhou） <zhouyewen_sx@qiyi.com>
2021-06-25 14:20:09 +0800

    Rewrite to functions, now support constants in front of min/max

--- src/package/solver.py
+++ src/package/solver.py
@@ -6,6 +6,34 @@ from itertools import product
 # TODO: what if the solution is a range?
 
 
+def get_value_term(equation):
+    return re.search(r"=\s*(\d+)", equation).group(1)
+
+
+def get_minmax_terms(equation):
+    """Return a list of tuples. """
+    return re.findall(r"(\+|\-)\s*(\d*)\s*\**\s*(min|max)(\([^\)]+\))",
+                      equation)
+
+
+def get_cons_var_terms(equation):
+    return re.findall(r"((\+|\-)\s*\d+(\*[^m])+)", equation)
+
+
+def gen_combs(minmax_terms):
+    """Return itertools.product object. """
+    lists = []
+    for e in minmax_terms:
+        match = re.findall(r"(\d+[\*a-z]*),\s*(\d+[\*a-z]*)", e[3])
+        lists.append(match[0])
+    return product(*lists)
+
+
+def gen_validate_eq(equation):
+    return (re.sub(r"\*([a-z]+)", convert_letters, equation)
+            .replace(r"=", "=="))
+
+
 def convert_letters(matchobj):
     m = matchobj.group()
     match_map = {m: "*result[0]"} if len(m) == 2 else {m: m}
@@ -22,36 +50,33 @@ def solve_sum_minmax(equation, var_name, low=0, high=1.0, decimal=False):
         high: float, the higher bound of the variable, exclusive
         decimal: bool,whether to return exact float value
     """
+    # TODO: if the equation starts with a -
     equation = f"+{equation}"
-    value = re.search(r"=\s*(\d+)", equation).group(1)
-    minmax_parts = re.findall(r"(\+|\-)\s*(min|max)(\([^\)]+\))", equation)
-    digit_parts = re.findall(r"((\+|\-)\s*\d+(\*[a-z])+)", equation)
-
-    lists = []
-    for e in minmax_parts:
-        match = re.findall(r"(\d+[\*a-z]*),\s+(\d+[\*a-z]*)", e[2])
-        lists.append(match[0])
-    products = product(*lists)
-
-    validate = (re.sub(r"\*([a-z]+)", convert_letters, equation)
-                .replace(r"=", "=="))
+    value_term = get_value_term(equation)
+    minmax_terms = get_minmax_terms(equation)
+    cons_var_terms = get_cons_var_terms(equation)
+    products = gen_combs(minmax_terms)
+    # print(list(products))
+    validate_eq = gen_validate_eq(equation)
     x = Symbol("x")
 
     for p in products:
         knitted = ""
-        for digit_part in digit_parts:
+        for digit_part in cons_var_terms:
             knitted += digit_part[0]
         for i, operand in enumerate(p):
-            operator = minmax_parts[i][0]
-            knitted += f"{operator}{operand}"
-        knitted += f"-{value}"
+            count = int(minmax_terms[i][1]) if minmax_terms[i][1] != "" else 1
+            for j in range(count):
+                operator = minmax_terms[i][0]
+                knitted += f"{operator}{operand}"
+        knitted += f"-{value_term}"
         knitted = knitted.replace(var_name, "x")
         result = solve(eval(knitted), x)
         if len(result) == 0:
             continue
         elif (result[0] <= low) or (result[0]) >= high:
             continue
-        if eval(validate):
+        if eval(validate_eq):
             if decimal:
                 return N(result[0])
             else:
@@ -60,5 +85,5 @@ def solve_sum_minmax(equation, var_name, low=0, high=1.0, decimal=False):
 
 
 if __name__ == "__main__":
-    eq = "800*a + 2*min(300, 400*a) = 1000"
+    eq = "800*a + 2*min(300, 400*a) + 3*max(400, 500*a) + min(400,500*a)= 1000"
     print(solve_sum_minmax(eq, "a"))
