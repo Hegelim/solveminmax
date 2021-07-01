55b2084d024396dde872fcbe3729a121c22322a5
Yewen Zhou <yewenzhou@berkeley.edu>
2021-07-01 21:58:01 +0800

    Finished! Now read to add in more test cases

--- src/package/solver.py
+++ src/package/solver.py
@@ -1,7 +1,7 @@
 import re
-from sympy import N, FiniteSet, Symbol, EmptySet, Complexes
+from sympy import S, N, FiniteSet, Symbol, EmptySet, Complexes, Interval
 from sympy.solvers import solveset
-from itertools import product
+import numpy as np
 # TODO: what if the solution is a range?
 # TODO: what if the equation starts with a -?
 # TODO: what if the interval is open on one end
@@ -9,86 +9,142 @@ from itertools import product
 # TODO: what if there are two numbers inside the min?
 
 
+def get_minmax_terms(equation):
+    """Return a list of tuples. """
+    return re.findall(r"(\+|\-)\s*(\d*)\s*\**\s*(min|max)(\([^\)]+\))", equation)
+
+
+def find_set_points(minmax_terms, var_name, low=0, high=1, left_open=True, right_open=True):
+    """Return a list of sorted set points.
+    There is at least one number. """
+    pts = set()
+    interval = Interval(low, high, left_open=left_open, right_open=right_open)
+    for term in minmax_terms:
+        match = re.findall(r"\s*(\d+).*,\s*(\d+)\s*", term[3])
+        left = float(match[0][0])
+        right = float(match[0][1])
+        splitted = term[3].split(",")
+        if var_name in splitted[0]:
+            pts.add(right/left)
+        elif var_name in splitted[1]:
+            pts.add(left/right)
+    return sorted(pts)
+
+
+def create_intervals(set_points, low=0, high=1, left_open=True, right_open=True):
+    """Create a list of intervals based on set_points.
+    a < x <= b. """
+    intervals = []
+    i = -1
+    j = 0
+    required_interval = Interval(low, high, left_open=left_open, right_open=right_open)
+    while i < len(set_points):
+        if i == -1:
+            interval = Interval(low, set_points[j], left_open=left_open, right_open=True)
+            if interval is not EmptySet:
+                intervals.append(interval)
+        elif j == len(set_points):
+            interval = Interval(set_points[i], high, left_open=True, right_open=right_open)
+            if interval is not EmptySet:
+                intervals.append(interval)
+        else:
+            intervals.append(Interval(set_points[i], set_points[j], left_open=True, right_open=True))
+        i += 1
+        j += 1
+    return intervals
+
+
+def get_validate_eq(equation):
+    return equation.replace("=", "==")
+
+
+def get_parts(eq):
+    return re.findall(r"[\+\-][^\+\-=]*", eq)
+
+
+def random_interval(interval):
+    return np.random.uniform(interval.start, interval.end)
+
+
+def get_left_right(minmax_tuple):
+    match = re.findall(r"\s*(\d+.*),\s*(\d+[^\)]*)\s*", minmax_tuple)
+    return match[0][0], match[0][1]
+
+
+def get_cons_var_terms(equation):
+    match_list = re.findall(r"(\+|\-)\s*(\d+)\s*\*\s*([a-z]+)", equation)
+    i = 0
+    n = len(match_list)
+    while i < len(match_list):
+        if "min" in match_list[i][2] or "max" in match_list[i][2]:
+            match_list.pop(i)
+            i -= 1
+            n -= 1
+        i += 1
+    return match_list
+
+
 def get_value_term(equation):
     return re.search(r"=\s*(\d+)", equation).group(1)
 
 
-def get_minmax_terms(equation):
-    """Return a list of tuples. """
-    return re.findall(r"(\+|\-)\s*(\d*)\s*\**\s*(min|max)(\([^\)]+\))",
-                      equation)
+def knit_solver(interval, minmax_terms, cons_var_terms, var_name):
+    solver = ""
+    for term in minmax_terms:
+        operator = term[0]
+        coef = 1 if term[1] == "" else float(term[1])
+        minmax = term[2]
+        minmax_tuple = term[3]
+        if var_name not in minmax_tuple:
+            val = eval(f"{minmax}{minmax_tuple}")
+            solver += f"{operator}{coef}*{val}"
+        else:
+            rand = random_interval(interval)
+            index = term[3].rfind(var_name)
+            replaced = term[3][:index] + "rand" + term[3][index+1:]
+            replaced = f"{minmax}{replaced}"
+            left, right = get_left_right(term[3])
+            if var_name not in left:
+                non_var_part = left
+                var_part = right
+            else:
+                non_var_part = right
+                var_part = left
+            try:
+                val = eval(replaced)
+            except Exception as e:
+                print(e)
+                return
+            if val != float(non_var_part):
+                solver += f"{operator}{coef}*{var_part}"
+            else:
+                solver += f"{operator}{coef}*{non_var_part}"
+    for term in cons_var_terms:
+        solver += f"{term[0]}{term[1]}*{term[2]}"
+    return solver
 
 
-def get_cons_var_terms(equation):
-    return re.findall(r"(\+|\-)\s*(\d+)\s*\*\s*([a-z]+)", equation)
-
-
-def gen_combs(minmax_terms):
-    """Return itertools.product object. """
-    lists = []
-    for e in minmax_terms:
-        match = re.findall(r"(\d+[\*a-z]*),\s*(\d+[\*a-z]*)", e[3])
-        lists.append(match[0])
-    return product(*lists)
-
-
-def gen_validate_eq(equation):
-    return (re.sub(r"\*([a-z]+)", convert_letters, equation)
-            .replace(r"=", "=="))
-
-
-def convert_letters(matchobj):
-    m = matchobj.group()
-    match_map = {m: "*next(iter(result))"} if len(m) == 2 else {m: m}
-    return match_map[m]
-
-
-def solve_sum_minmax(equation, var_name, low=0, high=1.0, decimal=False):
-    """Solve a sum of min equations numerically.
-
-    Args:
-        equation: string of min equation
-        var_name: char, the variable
-        low: float, the lower bound of the variable, exclusive
-        high: float, the higher bound of the variable, exclusive
-        decimal: bool,whether to return exact float value
-    """
-    # TODO: if the equation starts with a -
-    equation = f"+{equation}"
+def auto_solve(eq, var_name, low=0, high=1, left_open=True, right_open=True):
+    equation = f"+{eq}"
     value_term = get_value_term(equation)
     minmax_terms = get_minmax_terms(equation)
     cons_var_terms = get_cons_var_terms(equation)
-    products = gen_combs(minmax_terms)
-    validate_eq = gen_validate_eq(equation)
-    x = Symbol("x")
+    set_points = find_set_points(minmax_terms, var_name)
+    intervals = create_intervals(set_points)
+    results = []
+    for interval in intervals:
+        knitted_solver = knit_solver(intervals[0], minmax_terms, cons_var_terms, "a")
+        knitted_solver = f"{knitted_solver} - {value_term}"
+        a = Symbol("a")
+        result = solveset(eval(knitted_solver), a)
+        if result is S.Complexes:
+            return Interval(low, high, left_open=left_open, right_open=right_open)
+        elif list(result)[0] in interval:
+            results.append(result)
+        elif list(result)[0].evalf() == interval.start or list(result)[0].evalf() == interval.end:
+            a = np.random.uniform(interval.start, interval.end)
+            validate_eq = get_validate_eq(eq)
+            if eval(validate_eq):
+                return interval.union(result)
+    return results
 
-    for p in products:
-        knitted = ""
-        for digit_part in cons_var_terms:
-            knitted += digit_part[0]
-        for i, operand in enumerate(p):
-            count = int(minmax_terms[i][1]) if minmax_terms[i][1] != "" else 1
-            for j in range(count):
-                operator = minmax_terms[i][0]
-                knitted += f"{operator}{operand}"
-        knitted += f"-{value_term}"
-        knitted = knitted.replace(var_name, "x")
-        result = solveset(eval(knitted), x)
-        if result == EmptySet:
-            continue
-        elif result == Complexes:
-            continue
-        elif result == FiniteSet and \
-                (next(iter(result)) <= low) or (next(iter(result)) >= high):
-            continue
-        if eval(validate_eq):
-            if decimal:
-                return N(next(iter(result)))
-            else:
-                return next(iter(result))
-    return None
-
-
-if __name__ == "__main__":
-    eq = "min(300, 400*a) = 300"
-    print(solve_sum_minmax(eq, "a"))
--- tests/test_solve_sum_minmax.py
+++ tests/test_solve_sum_minmax.py
@@ -1,59 +1,53 @@
 from src.package import solver
+from sympy import FiniteSet, Interval, S
 
 
 class TestClass:
     def test_1(self):
         eq = 'min(400, 400*a) + min(400, 400*a) + min(0, 100*a) = 200'
-        assert solver.solve_sum_minmax(eq, "a") == 0.25
+        assert solver.auto_solve(eq, "a") == [FiniteSet(1/4)]
 
     def test_2(self):
         """If no solution. """
         eq = "min(400, 400*a) = 400"
-        assert solver.solve_sum_minmax(eq, "a") is None
+        assert solver.auto_solve(eq, "a") == []
 
     def test_3(self):
         eq = "min(400, 500*a) = 300"
-        assert solver.solve_sum_minmax(eq, "a").evalf() == 0.6
+        assert list(solver.auto_solve(eq, "a")[0])[0].evalf() == 0.6
 
     def test_4(self):
         """If a is on the left. """
         eq = "min(500*a, 400) = 300"
-        assert solver.solve_sum_minmax(eq, "a").evalf() == 0.6
+        assert list(solver.auto_solve(eq, "a")[0])[0].evalf() == 0.6
 
     def test_5(self):
         eq = "800*a + min(300, 400*a) + min(300, 400*a) = 1000"
-        assert solver.solve_sum_minmax(eq, "a").evalf() == 0.625
+        assert solver.auto_solve(eq, "a") == [FiniteSet(0.625)]
 
     def test_6(self):
         """If the equation has both min and max. """
         eq = "min(500, 600*a) + max(400, 500*a) = 500"
-        assert solver.solve_sum_minmax(eq, "a").evalf() == 1/6
+        assert list(solver.auto_solve(eq, "a")[0])[0].evalf() == 1/6
 
     def test_7(self):
         eq = "min(500, 600*a) - min(500, 600*a) = 0"
-        assert solver.solve_sum_minmax(eq, "a").evalf() == 5/6
+        assert solver.auto_solve(eq, "a") == Interval(0, 1, left_open=True, right_open=True)
 
     def test_8(self):
-        """If the user uses the variable x. """
-        eq = "800*x + min(300, 400*x) + min(300, 400*x) = 1000"
-        assert solver.solve_sum_minmax(eq, "x").evalf() == 0.625
-
-    def test_9(self):
-        """If the user uses the variable s. """
-        eq = "800*s + min(300, 400*s) + min(400*s, 400) = 1000"
-        assert solver.solve_sum_minmax(eq, "s").evalf() == 0.625
-
-    def test_10(self):
         """If has constants before min/max. """
         eq = "800*a + 2*min(300, 400*a) = 1000"
-        assert solver.solve_sum_minmax(eq, "a").evalf() == 0.625
+        assert solver.auto_solve(eq, "a") == [FiniteSet(5/8)]
 
-    # def test_11(self):
-    #     """If has infinite many solutions. """
-    #     eq = "min(400, 500*a) - min(400, 500*a) = 0"
-    #     assert solver.solve_sum_minmax(eq, "a") == Interval(0, 1)
-
-    def test_12(self):
+    def test_9(self):
         """If no solution. """
         eq = "min(400, 500*a) = 1000"
-        assert solver.solve_sum_minmax(eq, "a") is None
+        assert solver.auto_solve(eq, "a") == []
+
+    def test_10(self):
+        eq = "min(400, 600*a) = 400"
+        assert solver.auto_solve(eq, "a") == Interval(2/3, 1, left_open=False, right_open=True)
+
+    def test_11(self):
+        eq = "max(400*a, 600) = 600"
+        assert solver.auto_solve(eq, "a") == Interval(0, 1, left_open=True, right_open=True)
