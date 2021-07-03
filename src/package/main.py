8e8d57eb42d448687dbf43727411a9456e7fea93
Yewen Zhou <yewenzhou@berkeley.edu>
2021-07-03 13:12:58 +0800

    Added more test cases

--- src/package/main.py
+++ src/package/main.py
@@ -20,14 +20,15 @@ eqs = [
     "min(100, 400*a) + max(300, 400*a) = 400",
     "min(400*a, 100) = 500",
     "max(400*a, 100) = 50",
-    "max(300, 400*a) = 300"
+    "max(300, 400*a) = 300",
+    "20*a - 20*a = 0",
+    "20*a - 5*a = 0",
 ]
 
 
 if __name__ == "__main__":
     for i, eq in enumerate(eqs):
         print(f"Solving {i + 1}: {eq}...")
-        print(solver.get_cons_var_terms(eq))
-        # sol = solver.auto_solve(eq, "a")
-        # print(f"Solution is {sol}")
-        # print("==========================")
+        sol = solver.auto_solve(eq, "a")
+        print(f"Solution is {sol}")
+        print("==========================")
