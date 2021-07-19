f5cc55470131986fdaaef619923fce5b6a717a75
Hegelim <yewenzhou@berkeley.edu>
2021-07-19 10:37:50 +0800

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,9 @@ release = '0.1.3'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-              'autoapi.extension',
               'sphinx.ext.napoleon']
-autoapi_type = 'python'
-autoapi_dirs = ['../../src']
+# autoapi_type = 'python'
+# autoapi_dirs = ['../../src']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
